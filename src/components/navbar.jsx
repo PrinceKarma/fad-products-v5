753bd9d4f8b0a37b13b7eae58b8bfd59b20f65a3
vipin-g-abraham <vipinga96@gmail.com>
2024-09-30 23:42:06 -0400

    added itemCount to cart icon

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,21 +1,33 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 import { Link } from "react-router-dom";
 import { ShoppingCart } from 'phosphor-react';
 import { Heart } from 'phosphor-react';
-import { MagnifyingGlass } from 'phosphor-react';
+import { ShopContext } from "../context/shop-context";
+
 
 import "./navbar.css";
 
+
 export const Navbar = () => {
+    const { cartProducts } = useContext(ShopContext);
+    const cartTotalQuantity = Object.values(cartProducts).reduce((sum, count) => sum + count, 0);
     return (
         <div className="navbar">
             <div className="links">
-                {/* <Link to="/"> Shop </Link>
-                <Link to="/wishlist"><Heart size={32} /> </Link>
-                <Link to="/cart"> <ShoppingCart size={32} /> </Link> */}
+
                 <Link to="/"> <div className="navbar-element">Shop </div></Link>
-                <Link to="/wishlist"><div className="navbar-element"><Heart size={30} /> <div className="navbar-element-subtext">WishList</div></div></Link>
-                <Link to="/cart"> <div className="navbar-element"><ShoppingCart size={30} /> <div className="navbar-element-subtext">Cart</div></div></Link>
+                <Link to="/wishlist">
+                    <div className="navbar-element">
+                        <Heart size={30} />
+                        <div className="navbar-element-subtext">WishList</div>
+                    </div></Link>
+                <Link to="/cart">
+                    <div className="navbar-element">
+                        <span className="nav-icon"><ShoppingCart size={30} /></span>
+                        <span className="cart-badge" style={{ visibility: cartTotalQuantity > 0 ? "visible" : "hidden" }}>{cartTotalQuantity}</span>
+                        <div className="navbar-element-subtext">Cart</div>
+                    </div>
+                </Link>
 
             </div>
         </div>
