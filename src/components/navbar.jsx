b97441ae37e46dff6bef3ebaf46a8874db0b0c9a
vipin-g-abraham <vipinga96@gmail.com>
2024-09-30 23:43:43 -0400

    removed unused import

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import { ShoppingCart } from 'phosphor-react';
 import { Heart } from 'phosphor-react';
